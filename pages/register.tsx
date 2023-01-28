6ad6fc06cf1dfcd684a8e06b3775a37c09f256d2
Bobi2000 <bobko_2000@abv.bg>
2023-01-28 19:02:43 +0200

    Remed white spaces from login and register cus it blocks building on Vercel

--- pages/register.tsx
+++ pages/register.tsx
@@ -149,7 +149,7 @@ export default function Register() {
                 Create an account
               </button>
               <p className="text-sm font-light text-gray-500 dark:text-gray-400">
-                Already have an account?{" "}
+                Already have an account?
                 <Link
                   href="/login"
                   className="font-medium text-primary-600 hover:underline dark:text-primary-500"
