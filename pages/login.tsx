7dd9d83bb270add300a2cf91e7dd3fc34bae775e
Bobi2000 <bobko_2000@abv.bg>
2023-01-28 19:04:54 +0200

    Removing ' from page as it breaks Vercel build

--- pages/login.tsx
+++ pages/login.tsx
@@ -124,7 +124,7 @@ export default function Login() {
                 Login
               </button>
               <p className="text-sm font-light text-gray-500 dark:text-gray-400">
-                Don't have an account yet?
+                Don`&#39;`t have an account yet?
                 <Link
                   href="/register"
                   className="font-medium text-primary-600 hover:underline dark:text-primary-500"
